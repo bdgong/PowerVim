75db561dcb90853ded372adaeae8c3199640b0ba
bdgong <bardongong@163.com>
2017-07-08 11:35:14 +0800

    Add missing templates.

---
+++ templates/template.cpp
@@ -0,0 +1,13 @@
+/*
+ * Template
+ *
+ * date: Sun 11 Sep 2016 11:06:59 
+ * */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    // 
+    return 0;
+}
+
---
+++ templates/template.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python 
+# -*- coding: utf-8 -*-
+# 
+
+def main():
+    print('Hello')
+
+if __name__ == '__main__':
+    main()
+
